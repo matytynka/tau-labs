b52336784dfb8ba6822d560d55428ef7a9a5dd02
matytynka <matytynka@o2.pl>
2021-03-09 18:39:07 +0100

    lab01 added

---
+++ lab01/src/main/java/lab01/Counter.java
@@ -0,0 +1,29 @@
+package lab01;
+
+public class Counter {
+    private int counter;
+    public Counter(int number) {
+        counter = number;
+    }
+    public void increase(int value){
+        if (counter + value <= 100) counter = counter + value;
+        else {
+            System.out.println("Value is too high");
+            counter = 100;
+        }
+    }
+    public void decrease(int value){
+        if (counter - value >= 0) counter = counter - value;
+        else {
+            System.out.println("Value is too low");
+            counter = 0;
+        }
+    }
+    public boolean check(){
+        System.out.println("Value is perfect");
+        return counter == 50;
+    }
+    public int getCounter() {
+        return counter;
+    }
+}
