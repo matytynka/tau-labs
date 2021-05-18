a5c1a0e5a0e1762584b8d7396afa244f7a2f6185
matytynka <matytynka@o2.pl>
2021-05-18 13:25:55 +0200

    added excercise 3 with cookies to pyszne_test

--- lab08/pyszne_test.py
+++ lab08/pyszne_test.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 pyszne_address = 'https://www.pyszne.pl/'
 chrome = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 chrome.maximize_window()
 chrome.get(pyszne_address)
+
+#Zadanie dodatkowe cookie
+cookie_button = WebDriverWait(chrome, 3).until(expected_conditions.presence_of_element_located(
+    (By.XPATH, '//*[@class="cc-banner"]/section[1]/article[1]/button')))
+cookie_button.click()
+
 time.sleep(1)
 pyszne = chrome.find_element_by_xpath('//*[@id="imysearchstring"]')
 pyszne.click()
